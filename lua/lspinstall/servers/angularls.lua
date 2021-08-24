00b7c87d6f1e35589d655a45cc18afaf6620b17a
nazo6 <53177992+nazo6@users.noreply.github.com>
2021-08-24 23:47:45 +0900

    Fix angularls.

--- lua/lspinstall/servers/angularls.lua
+++ lua/lspinstall/servers/angularls.lua
@@ -1,8 +1,25 @@
+local util = require "lspinstall/util"
 local config = require("lspinstall/util").extract_config "angularls"
 local npm = require("lspinstall/helpers").npm
 
-local package_name = "@angular/language-server"
-config.default_config.cmd[1] = npm.bin_path(config.default_config.cmd[1])
-return vim.tbl_extend("error", config, {
-  install_script = npm.install_script(package_name),
+local cmd_name = config.default_config.cmd[1]
+local node_module_path = util.install_path "angularls" .. "/node_modules/"
+
+local prefix = ""
+if util.is_windows() then
+  prefix = ".cmd"
+end
+
+config.default_config.cmd[1] = npm.bin_path(cmd_name)
+config.default_config.cmd[6] = node_module_path
+
+return vim.tbl_extend("force", config, {
+  install_script = function()
+    return npm.install_script "@angular/language-server" .. [[
+    ]] .. npm.install_script "@angular/language-service"
+  end,
+  on_new_config = function(new_config)
+    new_config.cmd[1] = node_module_path .. ".bin/" .. cmd_name .. prefix
+    new_config.cmd[6] = node_module_path
+  end,
 })
