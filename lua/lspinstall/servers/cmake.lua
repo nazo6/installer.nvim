70dbd26cc740a976467be449d052aa5eb149c471
brian.orwe <brian.orwe@gmail.com>
2021-09-10 01:54:02 +0300

    remove old commented out code from cmake.lua

--- lua/lspinstall/servers/cmake.lua
+++ lua/lspinstall/servers/cmake.lua
@@ -1,7 +1,3 @@
-
---return vim.tbl_extend("error", config, {
---  install_script = script_to_use,
---})
 return require("lspinstall/helpers").pip.builder {
   lang = "cmake",
   inherit_lspconfig = true,
