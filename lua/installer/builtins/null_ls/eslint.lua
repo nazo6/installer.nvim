e66c2b7dd93bd6ac8ce78b4723470227d4ae91a4
nazo6 <53177992+nazo6@users.noreply.github.com>
2021-09-20 22:12:48 +0900

    Fix eslint

--- lua/installer/builtins/null_ls/eslint.lua
+++ lua/installer/builtins/null_ls/eslint.lua
@@ -3,5 +3,5 @@ local builder = require("installer/integrations/null_ls/helpers").npm.builder
 return builder({
   name = "eslint",
   install_package = "eslint",
-  type = { "formatting", "code_actions", "diagnostics" },
+  type = { "formatting", "diagnostics" },
 })
